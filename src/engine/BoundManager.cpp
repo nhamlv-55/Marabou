d8925852e1962ad11d9d8a81f7edb7d59090e7e0
Nham Le <nv3le@uwaterloo.ca>
2023-02-06 15:37:48 +0000

    try to use a different gte for checking bound consistency

--- src/engine/BoundManager.cpp
+++ src/engine/BoundManager.cpp
@@ -21,38 +21,30 @@
 #include "Tableau.h"
 #include "Tightening.h"
 #include "MarabouError.h"
-
+#include "GlobalConfiguration.h"
 using namespace CVC4::context;
 
-BoundManager::BoundManager( Context &context )
-    : _context( context )
-    , _size( 0 )
-    , _allocated( 0 )
-    , _tableau( nullptr )
-    , _rowBoundTightener( nullptr )
-    , _consistentBounds( &_context )
-    , _firstInconsistentTightening( 0, 0.0, Tightening::LB )
-    , _lowerBounds( nullptr )
-    , _upperBounds( nullptr )
+BoundManager::BoundManager(Context &context)
+    : _context(context), _size(0), _allocated(0), _tableau(nullptr), _rowBoundTightener(nullptr), _consistentBounds(&_context), _firstInconsistentTightening(0, 0.0, Tightening::LB), _lowerBounds(nullptr), _upperBounds(nullptr)
 {
     _consistentBounds = true;
 };
 
 BoundManager::~BoundManager()
 {
-    if ( _lowerBounds )
+    if (_lowerBounds)
     {
         delete[] _lowerBounds;
         _lowerBounds = nullptr;
     }
 
-    if ( _upperBounds )
+    if (_upperBounds)
     {
         delete[] _upperBounds;
         _upperBounds = nullptr;
     }
 
-    for ( unsigned i = 0; i < _size; ++i )
+    for (unsigned i = 0; i < _size; ++i)
     {
         _storedLowerBounds[i]->deleteSelf();
         _storedUpperBounds[i]->deleteSelf();
@@ -61,66 +53,66 @@ BoundManager::~BoundManager()
     }
 };
 
-void BoundManager::initialize( unsigned numberOfVariables )
+void BoundManager::initialize(unsigned numberOfVariables)
 {
-    ASSERT( _size == 0 );
+    ASSERT(_size == 0);
 
-    allocateLocalBounds( numberOfVariables );
+    allocateLocalBounds(numberOfVariables);
 
-    for ( unsigned i = 0; i < numberOfVariables; ++i )
+    for (unsigned i = 0; i < numberOfVariables; ++i)
         registerNewVariable();
 
-    ASSERT( _size == numberOfVariables );
-    ASSERT( _allocated == numberOfVariables );
+    ASSERT(_size == numberOfVariables);
+    ASSERT(_allocated == numberOfVariables);
 }
 
-void BoundManager::allocateLocalBounds( unsigned size )
+void BoundManager::allocateLocalBounds(unsigned size)
 {
     _lowerBounds = new double[size];
-    if ( !_lowerBounds )
-        throw MarabouError( MarabouError::ALLOCATION_FAILED, "BoundManager::lowerBounds" );
-    std::fill_n( _lowerBounds, size, FloatUtils::negativeInfinity() );
+    if (!_lowerBounds)
+        throw MarabouError(MarabouError::ALLOCATION_FAILED, "BoundManager::lowerBounds");
+    std::fill_n(_lowerBounds, size, FloatUtils::negativeInfinity());
 
     _upperBounds = new double[size];
-    if ( !_upperBounds )
-        throw MarabouError( MarabouError::ALLOCATION_FAILED, "BoundManager::upperBounds" );
-    std::fill_n( _upperBounds, size, FloatUtils::infinity() );
+    if (!_upperBounds)
+        throw MarabouError(MarabouError::ALLOCATION_FAILED, "BoundManager::upperBounds");
+    std::fill_n(_upperBounds, size, FloatUtils::infinity());
     _allocated = size;
 
-    if ( _tableau )
-      _tableau->setBoundsPointers( _lowerBounds, _upperBounds );
+    if (_tableau)
+        _tableau->setBoundsPointers(_lowerBounds, _upperBounds);
 
-    if ( _rowBoundTightener )
-      _rowBoundTightener->setBoundsPointers( _lowerBounds, _upperBounds );
+    if (_rowBoundTightener)
+        _rowBoundTightener->setBoundsPointers(_lowerBounds, _upperBounds);
 }
 
 unsigned BoundManager::registerNewVariable()
 {
-    ASSERT( _size == _storedLowerBounds.size() );
-    ASSERT( _size == _storedUpperBounds.size() );
-    ASSERT( _size == _tightenedLower.size() );
-    ASSERT( _size == _tightenedUpper.size() );
+    ASSERT(_size == _storedLowerBounds.size());
+    ASSERT(_size == _storedUpperBounds.size());
+    ASSERT(_size == _tightenedLower.size());
+    ASSERT(_size == _tightenedUpper.size());
 
     unsigned newVar = _size++;
 
-    if ( _allocated < _size )
+    if (_allocated < _size)
     {
-      double * oldLowerBounds = _upperBounds;
-      double * oldUpperBounds = _lowerBounds;
+        double *oldLowerBounds = _upperBounds;
+        double *oldUpperBounds = _lowerBounds;
 
-      allocateLocalBounds( 2*_allocated );
-      std::memcpy( _lowerBounds, oldLowerBounds, _allocated );
-      std::memcpy( _upperBounds, oldUpperBounds, _allocated );
-      _allocated *= 2;
+        allocateLocalBounds(2 * _allocated);
+        std::memcpy(_lowerBounds, oldLowerBounds, _allocated);
+        std::memcpy(_upperBounds, oldUpperBounds, _allocated);
+        _allocated *= 2;
 
-      delete[] oldLowerBounds;
-      delete[] oldUpperBounds;
+        delete[] oldLowerBounds;
+        delete[] oldUpperBounds;
     }
 
-    _storedLowerBounds.append( new ( true ) CDO<double>( &_context ) );
-    _storedUpperBounds.append( new ( true ) CDO<double>( &_context ) );
-    _tightenedLower.append( new ( true ) CDO<bool>( &_context ) );
-    _tightenedUpper.append( new ( true ) CDO<bool>( &_context ) );
+    _storedLowerBounds.append(new (true) CDO<double>(&_context));
+    _storedUpperBounds.append(new (true) CDO<double>(&_context));
+    _tightenedLower.append(new (true) CDO<bool>(&_context));
+    _tightenedUpper.append(new (true) CDO<bool>(&_context));
 
     *_storedLowerBounds[newVar] = FloatUtils::negativeInfinity();
     *_storedUpperBounds[newVar] = FloatUtils::infinity();
@@ -135,113 +127,113 @@ unsigned BoundManager::getNumberOfVariables() const
     return _size;
 }
 
-bool BoundManager::tightenLowerBound( unsigned variable, double value )
+bool BoundManager::tightenLowerBound(unsigned variable, double value)
 {
-    bool tightened = setLowerBound( variable, value );
-    if ( tightened && _tableau != nullptr )
-        _tableau->updateVariableToComplyWithLowerBoundUpdate( variable, value );
+    bool tightened = setLowerBound(variable, value);
+    if (tightened && _tableau != nullptr)
+        _tableau->updateVariableToComplyWithLowerBoundUpdate(variable, value);
     return tightened;
 }
 
-bool BoundManager::tightenUpperBound( unsigned variable, double value )
+bool BoundManager::tightenUpperBound(unsigned variable, double value)
 {
-    bool tightened = setUpperBound( variable, value );
-    if ( tightened && _tableau != nullptr )
-        _tableau->updateVariableToComplyWithUpperBoundUpdate( variable, value );
+    bool tightened = setUpperBound(variable, value);
+    if (tightened && _tableau != nullptr)
+        _tableau->updateVariableToComplyWithUpperBoundUpdate(variable, value);
     return tightened;
 }
 
-void BoundManager::recordInconsistentBound( unsigned variable, double value, Tightening::BoundType type )
+void BoundManager::recordInconsistentBound(unsigned variable, double value, Tightening::BoundType type)
 {
-  if ( _consistentBounds )
-  {
-    _consistentBounds = false;
-    _firstInconsistentTightening = Tightening( variable, value, type );
-  }
+    if (_consistentBounds)
+    {
+        std::cerr << "INCONSTISTENT BOUND<<<<<<<<  " << variable << " .    " << value << " .  " << type << std::endl;
+        _consistentBounds = false;
+        _firstInconsistentTightening = Tightening(variable, value, type);
+    }
 }
 
-bool BoundManager::setLowerBound( unsigned variable, double value )
+bool BoundManager::setLowerBound(unsigned variable, double value)
 {
-    ASSERT( variable < _size );
-    if ( value > _lowerBounds[variable] )
+    ASSERT(variable < _size);
+    if (value > _lowerBounds[variable])
     {
         _lowerBounds[variable] = value;
         *_tightenedLower[variable] = true;
-        if ( !consistentBounds( variable ) )
-            recordInconsistentBound( variable, value, Tightening::LB );
+        if (!consistentBounds(variable))
+            recordInconsistentBound(variable, value, Tightening::LB);
         return true;
     }
     return false;
 }
 
-
-bool BoundManager::setUpperBound( unsigned variable, double value )
+bool BoundManager::setUpperBound(unsigned variable, double value)
 {
-    ASSERT( variable < _size );
-    if ( value < _upperBounds[variable] )
+    ASSERT(variable < _size);
+    if (value < _upperBounds[variable])
     {
         _upperBounds[variable] = value;
         *_tightenedUpper[variable] = true;
-        if ( !consistentBounds( variable ) )
-          recordInconsistentBound( variable, value, Tightening::UB );
+        if (!consistentBounds(variable))
+            recordInconsistentBound(variable, value, Tightening::UB);
         return true;
     }
     return false;
 }
 
-double BoundManager::getLowerBound( unsigned variable ) const
+double BoundManager::getLowerBound(unsigned variable) const
 {
-    ASSERT( variable < _size );
+    ASSERT(variable < _size);
     return _lowerBounds[variable];
 }
 
-double BoundManager::getUpperBound( unsigned variable ) const
+double BoundManager::getUpperBound(unsigned variable) const
 {
-    ASSERT( variable < _size );
+    ASSERT(variable < _size);
     return _upperBounds[variable];
 }
 
-const double * BoundManager::getLowerBounds() const
+const double *BoundManager::getLowerBounds() const
 {
     return _lowerBounds;
 }
 
-const double * BoundManager::getUpperBounds() const
+const double *BoundManager::getUpperBounds() const
 {
     return _upperBounds;
 }
 
 void BoundManager::storeLocalBounds()
 {
-    for ( unsigned i = 0; i < _size; ++i )
+    for (unsigned i = 0; i < _size; ++i)
     {
-      *_storedLowerBounds[i]=_lowerBounds[i];
-      *_storedUpperBounds[i]=_upperBounds[i];
+        *_storedLowerBounds[i] = _lowerBounds[i];
+        *_storedUpperBounds[i] = _upperBounds[i];
     }
 }
 
 void BoundManager::restoreLocalBounds()
 {
-    for ( unsigned i = 0; i < _size; ++i )
+    for (unsigned i = 0; i < _size; ++i)
     {
-      _lowerBounds[i]=*_storedLowerBounds[i];
-      _upperBounds[i]=*_storedUpperBounds[i];
+        _lowerBounds[i] = *_storedLowerBounds[i];
+        _upperBounds[i] = *_storedUpperBounds[i];
     }
 }
 
-void BoundManager::getTightenings( List<Tightening> &tightenings )
+void BoundManager::getTightenings(List<Tightening> &tightenings)
 {
-    for ( unsigned i = 0; i < _size; ++i )
+    for (unsigned i = 0; i < _size; ++i)
     {
-        if ( *_tightenedLower[i] )
+        if (*_tightenedLower[i])
         {
-            tightenings.append( Tightening( i, _lowerBounds[i], Tightening::LB ) );
+            tightenings.append(Tightening(i, _lowerBounds[i], Tightening::LB));
             *_tightenedLower[i] = false;
         }
 
-        if ( *_tightenedUpper[i] )
+        if (*_tightenedUpper[i])
         {
-            tightenings.append( Tightening( i, _upperBounds[i], Tightening::UB ) );
+            tightenings.append(Tightening(i, _upperBounds[i], Tightening::UB));
             *_tightenedUpper[i] = false;
         }
     }
@@ -249,28 +241,28 @@ void BoundManager::getTightenings( List<Tightening> &tightenings )
 
 void BoundManager::clearTightenings()
 {
-    for ( unsigned i = 0; i < _size; ++i )
+    for (unsigned i = 0; i < _size; ++i)
     {
-      *_tightenedLower[i]=false;
-      *_tightenedUpper[i]=false;
+        *_tightenedLower[i] = false;
+        *_tightenedUpper[i] = false;
     }
 }
 
 void BoundManager::propagateTightenings()
 {
-    for ( unsigned i = 0; i < _size; ++i )
+    for (unsigned i = 0; i < _size; ++i)
     {
-      if ( *_tightenedLower[i] )
-      {
-        _tableau->notifyLowerBound( i, getLowerBound( i ) );
-        *_tightenedLower[i] = false;
-      }
+        if (*_tightenedLower[i])
+        {
+            _tableau->notifyLowerBound(i, getLowerBound(i));
+            *_tightenedLower[i] = false;
+        }
 
-      if ( *_tightenedUpper[i] )
-      {
-        _tableau->notifyUpperBound( i, getUpperBound( i ) );
-        *_tightenedUpper[i] = false;
-      }
+        if (*_tightenedUpper[i])
+        {
+            _tableau->notifyUpperBound(i, getUpperBound(i));
+            *_tightenedUpper[i] = false;
+        }
     }
 }
 
@@ -279,20 +271,54 @@ bool BoundManager::consistentBounds() const
     return _consistentBounds;
 }
 
-bool BoundManager::consistentBounds( unsigned variable ) const
+bool BoundManager::consistentBounds(unsigned variable) const
 {
-    ASSERT( variable < _size );
-    return FloatUtils::gte( getUpperBound( variable ), getLowerBound( variable ) );
+    ASSERT(variable < _size);
+    double ub = getUpperBound(variable);
+    double lb = getLowerBound(variable);
+    std::cerr << variable << " " << ub << " . " << lb << " . " << ub - lb << std::endl;
+    
+    
+    bool isConsistent = FloatUtils::gte(ub, lb);
+    return isConsistent;
+
+
+    if (FloatUtils::isZero(ub) && FloatUtils::isZero(lb))
+    {
+        std::cerr << "SNAPPED TO 0" << std::endl;
+        _upperBounds[variable] = 0;
+        _lowerBounds[variable] = 0;
+        return true;
+    }
+    if (!isConsistent)
+    {
+        bool isAlmostConsistent = FloatUtils::gte_consistentBounds(ub, lb, GlobalConfiguration::DEFAULT_EPSILON_FOR_COMPARISONS * 100);
+        std::cerr << "CURRENT VAR" << variable << std::endl;
+        std::cerr << "CURRENT UPPER:" << ub << std::endl;
+        std::cerr << "CURRENT LOWER:" << lb << std::endl;
+        std::cerr << "TRY TO SNAP TO ub!..." << std::endl;
+
+        double zero_thres = GlobalConfiguration::DEFAULT_EPSILON_FOR_COMPARISONS;
+        if (isAlmostConsistent)
+        {
+
+            std::cerr << "SNAPPED TO UB!" << std::endl;
+            _upperBounds[variable] = ub;
+            _lowerBounds[variable] = ub;
+            return true;
+        }
+    }
+    return isConsistent;
 }
 
-void BoundManager::registerTableau( ITableau *ptrTableau )
+void BoundManager::registerTableau(ITableau *ptrTableau)
 {
-    ASSERT( _tableau == nullptr );
+    ASSERT(_tableau == nullptr);
     _tableau = ptrTableau;
 }
 
-void BoundManager::registerRowBoundTightener( IRowBoundTightener *ptrRowBoundTightener )
+void BoundManager::registerRowBoundTightener(IRowBoundTightener *ptrRowBoundTightener)
 {
-    ASSERT( _rowBoundTightener == nullptr );
+    ASSERT(_rowBoundTightener == nullptr);
     _rowBoundTightener = ptrRowBoundTightener;
 }
